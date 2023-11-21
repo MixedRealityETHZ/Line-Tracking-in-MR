749ea4d5c8cd3e09bdce2d27df0d7ca8d50be4bb
KimSinjeong <shinjeong.kim.99@gmail.com>
2023-11-21 11:17:25 +0100

    Working Line Detection Pipeline

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-import src.main_app as main_app
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics.texture import Texture
+from kivy.uix.camera import Camera
+from kivy.lang import Builder
+import numpy as np
+import cv2
+import pyelsed
 
-# Code suggestion (for the developers).
-# The `main.py` will serve two purposes:
-# Define and/or create calls to:
-#   a) the main application, which is currently done with `main_app.run()`.
-#   b) potential input arguments --from the command console or otherwise.
+Builder.load_file("myapplayout.kv")
 
-# I recommend that any code file regarding the software's logic
-# should be included in the `src` folder (or any of its subdirectories).
-# I encourage people to follow this, so that we do not
-# stumble upon unexpected errors when running `main.py`,
-# due to the importing system of the Python language.
+class AndroidCamera(Camera):
+    camera_resolution = (640, 480)
+    counter = 0
 
-# Other folders may exist in the same hierarchical level (i.e. the root)
-# of the project directory tree,
-# as long as it is independent of anything relating to the logic of the program.
-# Such examples include images, videos, (raw) data etc.
+    def __init__(self, **kwargs):
+        super(AndroidCamera, self).__init__(**kwargs)
+        
+        self.texture = Texture.create(size=np.flip(self.camera_resolution))
+        self.texture_size = list(self.texture.size)
 
-def main():
-    main_app.run()
+    def on_tex(self, *l):
+        if self._camera._buffer is None:
+            return None
+        frame = self.frame_from_buf()
+        self.frame_to_screen(frame)
+        super(AndroidCamera, self).on_tex(*l)
 
+    def frame_from_buf(self):
+        w, h = self.resolution
+        frame = np.frombuffer(self._camera._buffer.tostring(), 'uint8').reshape((h + h // 2, w))
+        frame_bgr = cv2.cvtColor(frame, 93)
+        return frame_bgr
+
+    def frame_to_screen(self, frame):
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        self.counter += 1
+        gray = cv2.cvtColor(frame_rgb, cv2.COLOR_RGBA2GRAY)
+        segments, scores = pyelsed.detect(gray)
+
+        for s in segments.astype(np.int32):
+            cv2.line(frame_rgb, (s[0], s[1]), (s[2], s[3]), (0, 255, 0), 1, cv2.LINE_AA)
+            
+        self.texture.blit_buffer(frame_rgb.reshape(-1), colorfmt='rgba', bufferfmt='ubyte')
+
+class MyLayout(BoxLayout):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return MyLayout()
 
 if __name__ == '__main__':
-    main()
+    MyApp().run()
