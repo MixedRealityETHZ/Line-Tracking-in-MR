24e2068b1dde3e63aa5d007673beaf876085f6b4
AidynUbingazhibov <aidyn.ubingazhibov@nu.edu.kz>
2023-12-04 01:17:43 +0100

    pybind

---
+++ src/test_linefeaturetracker.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include "linefeature_tracker.h"
+#define VIS_TRACK false
+
+int main() {
+    LineFeatureTracker trackerData;
+
+    cv::Mat frame1, frame2;
+    int pub_count = 1;
+    
+    VideoCapture capture("../tests/test_video.mp4");
+    Mat frame;
+
+    if( !capture.isOpened() )
+        throw "Error when reading steam_avi";
+    
+    capture >> frame; // Read the first frame
+    if (frame.empty())
+        throw "Error reading the first frame";
+
+    cv::VideoWriter outputVideo = cv::VideoWriter("output_video.avi", cv::VideoWriter::fourcc('M', 'J', 'P', 'G'),\
+     30, cv::Size(frame.cols, frame.rows));
+    
+    for( ; ; )
+    {
+        trackerData.readImage(frame);
+
+        cv::Mat tmp_img;
+        cv::cvtColor(trackerData.cur_img->img, tmp_img, CV_GRAY2RGB);
+
+        const vector<Line> &un_lines = trackerData.cur_img->vecLine;
+        for (unsigned int j = 0; j < trackerData.cur_img->vecLine.size(); j++)
+        {
+            if (trackerData.cur_img->success[j] != -1)
+                continue;
+
+            Point2f start = un_lines[j].keyPoint[1];
+            Point2f end = un_lines[j].keyPoint[un_lines[j].keyPoint.size() - 1];
+            Point2f start1 = un_lines[j].StartPt;
+            Point2f end1 = un_lines[j].EndPt;
+            
+            cv::line(tmp_img, start, end, cv::Scalar(0, 255, 0), 2, LINE_AA);
+            cv::line(tmp_img, start1, end1, cv::Scalar(0, 255, 0), 1, LINE_AA);
+            cv::putText(tmp_img, std::to_string(trackerData.cur_img->lineID[j]), trackerData.cur_img->vecLine[j].keyPoint[0], cv::FONT_HERSHEY_SIMPLEX, 0.45, CV_RGB(255, 230, 0), 1.8);
+        }
+        for (unsigned int m = 0; m < trackerData.cur_img->vecLine.size(); m++)
+        {
+            if (trackerData.cur_img->success[m] == -1)
+                break;
+
+            Point2f start = un_lines[m].keyPoint[1];
+            Point2f end = un_lines[m].keyPoint[un_lines[m].keyPoint.size() - 1];
+            Point2f start1 = un_lines[m].StartPt;
+            Point2f end1 = un_lines[m].EndPt;
+            
+            cv::line(tmp_img, start, end, cv::Scalar(255, 0, 0), 2, LINE_AA);
+            cv::line(tmp_img, start1, end1, cv::Scalar(255, 0, 0), 1, LINE_AA);
+            cv::putText(tmp_img, std::to_string(trackerData.cur_img->lineID[m]), trackerData.cur_img->vecLine[m].keyPoint[0], cv::FONT_HERSHEY_SIMPLEX, 0.45, CV_RGB(255, 230, 0), 1.8);
+        }
+
+        cv::Mat detect_img;
+        cv::cvtColor(trackerData.cur_img->img, detect_img, CV_GRAY2RGB);
+
+        if (VIS_TRACK){
+            LineRecord lines;
+
+            for (unsigned int m = 0; m < trackerData.cur_img->lineRec.size(); m++){
+                lines = trackerData.cur_img->lineRec[m];
+                if (lines.size() > 20) lines.erase(lines.begin(), lines.end() - 20);
+                
+                for (unsigned int j = 0; j < lines.size(); j++)
+                {
+                    Point2f start = lines[j].start;
+                    Point2f end = lines[j].end;
+                    if (j == lines.size() - 1)
+                        cv::line(detect_img, start, end, cv::Scalar(0, 255, 0), 2, LINE_AA);
+                    else
+                        cv::line(detect_img, start, end, cv::Scalar(0, 0, 55 + 10*j), 1, LINE_AA);
+                }
+            }
+            cv::imshow("img", detect_img);
+            cv::waitKey(20);
+            outputVideo.write(detect_img);
+        } else{
+            cv::imshow("img", tmp_img);
+            cv::waitKey(20);
+            outputVideo.write(tmp_img);
+        }
+        capture >> frame;
+
+        if(frame.empty())
+            break;
+    }
+
+    outputVideo.release();
+    waitKey(0);
+
+    return 0;
+}
---
+++ test.py
@@ -0,0 +1,97 @@
+import line_tracker
+import cv2
+import numpy as np
+VIS_TRACK = False
+
+if __name__ == '__main__':
+    cap = cv2.VideoCapture("tests/test_video.mp4")
+
+    if not cap.isOpened():
+        print("Error opening video file")
+        exit()
+    
+    ret, frame = cap.read()
+    if not ret:
+        print("Error reading the first frame")
+        exit()
+
+    output_video = cv2.VideoWriter("output_video.avi", cv2.VideoWriter_fourcc(*'MJPG'), 30, (frame.shape[1], frame.shape[0]))
+    trackerData = line_tracker.LineFeatureTracker()
+    
+    # Read and display frames until the video ends
+    while True:
+        #print(frame)
+        trackerData.readImage(frame)
+        cur_img = trackerData.getCurImg()
+        img = cur_img.img
+
+        tmp_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+        un_lines = cur_img.vecLine
+
+        lineIDs = cur_img.lineID
+
+        successes = cur_img.success
+
+        for j in range(len(un_lines)):
+            if successes[j] != -1:
+                continue
+            
+            start = un_lines[j]["keyPoint"][1]
+            end = un_lines[j]["keyPoint"][-1]
+            start1 = un_lines[j]["StartPt"]
+            end1 = un_lines[j]["EndPt"]
+
+            cv2.line(tmp_img, start, end, (0, 255, 0), 2, cv2.LINE_AA)
+            cv2.line(tmp_img, start1, end1, (0, 255, 0), 1, cv2.LINE_AA)
+            cv2.putText(tmp_img, str(lineIDs[j]), un_lines[j]["keyPoint"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 230, 0), 1, cv2.LINE_AA)
+        
+        un_lines = cur_img.vecLine
+        for m in range(len(un_lines)):
+            if successes[m] == -1:
+                break
+
+            start = un_lines[m]["keyPoint"][1]
+            end = un_lines[m]["keyPoint"][-1]
+            start1 = un_lines[m]["StartPt"]
+            end1 = un_lines[m]["EndPt"]
+
+            cv2.line(tmp_img, start, end, (255, 0, 0), 2, cv2.LINE_AA)
+            cv2.line(tmp_img, start1, end1, (255, 0, 0), 1, cv2.LINE_AA)
+            cv2.putText(tmp_img, str(lineIDs[m]), un_lines[m]["keyPoint"][0], cv2.FONT_HERSHEY_SIMPLEX, 0.45, (255, 230, 0), 1, cv2.LINE_AA)
+        
+        if VIS_TRACK:
+            detect_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+
+            lineRec = cur_img.lineRec
+
+            for m in range(len(lineRec)):
+                lines = lineRec[m]
+                
+                if len(lines) > 20:
+                    lines = lines[-20:]
+                
+                for j in range(len(lines)):
+                    start = lines[j]["start"]
+                    end = lines[j]["end"]
+
+                    if j == len(lines) - 1:
+                        cv2.line(detect_img, start, end, (0, 255, 0), 2, cv2.LINE_AA)
+                    else:
+                        cv2.line(detect_img, start, end, (0, 0, 55 + 10 * j), 1, cv2.LINE_AA)
+            
+            cv2.imshow("img", detect_img)
+            cv2.waitKey(20)
+            output_video.write(detect_img)
+        else:            
+            cv2.imshow("img", tmp_img)
+            cv2.waitKey(20)
+            output_video.write(tmp_img)
+        
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+    
+    output_video.release()
+    cv2.destroyAllWindows()
